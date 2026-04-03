7364aa7e87b335925a4f3d776b7e6f496c94fc26
LAPTOP-40EAPMHR\kanya <patipanc@metis.co.th>
2026-04-03 15:29:37 +0700

    initial asp.net core backend code

---
+++ backend-member/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using backend_member.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_member.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ResponseDto _response;
+        public AuthController()
+        {
+            _response = new ResponseDto();
+        }
+
+        [HttpPost("login")]
+        public ResponseDto Login()
+        {
+            _response.message = "login Function";
+            return _response;
+        }
+
+        [HttpPost("register")]
+        public ResponseDto Register()
+        {
+            _response.message = "Register Function";
+            return _response;
+        }
+
+        [HttpPost("logout")]
+        public ResponseDto Logout()
+        {
+            _response.message = "Logout Function";
+            return _response;
+        }
+    }
+}
---
+++ backend-member/Models/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace backend_member.Models
+{
+    public class ResponseDto
+    {
+        public bool? isSuccess { get; set; } = true;
+        public string? message { get; set; } = "";
+        public object? result { get; set; } = null;
+    }
+}

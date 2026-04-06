5064784c294a9331f6f72b8ef67ca2264e9fca32
LAPTOP-40EAPMHR\kanya <patipanc@metis.co.th>
2026-04-06 12:27:25 +0700

    no message

--- frontend-member/src/environment/environment.ts
+++ frontend-member/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
    environmentName: 'DEV',
    buildNumber: 'V. 1.0.0',
-   apiUrl: '/api-auth', // Uses proxy to https://thaibevauth.up.railway.app 
+   apiUrl: '/api-auth', // Uses proxy to https://thaibevauth.up.railway.app  /api-auth
    logLevel: 'debug'
 };
